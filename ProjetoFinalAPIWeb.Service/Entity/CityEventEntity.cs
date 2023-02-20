eed70710383f37a2b21518085ab50d80276eb562
JoaoA <joaootsugua@gmail.com>
2023-02-19 22:32:30 -0300

    Corrigindo algumas props para nullable

--- ProjetoFinalAPIWeb.Service/Entity/CityEventEntity.cs
+++ ProjetoFinalAPIWeb.Service/Entity/CityEventEntity.cs
@@ -4,19 +4,19 @@ namespace ProjetoFinalAPIWeb.Service.Entity
 {
     public class CityEventEntity
     {
-        public long? IdEvent { get; set; }
+        public long IdEvent { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
         [StringLength(150)]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         [DataType(DataType.Date)]
         public DateTime DateHourEvent { get; set; }
         [StringLength(50, MinimumLength = 3)]
         public string Local { get; set; }
         [StringLength(50, MinimumLength = 3)]
-        public string Address { get; set; }                
-        public decimal Price { get; set; }
+        public string? Address { get; set; }                
+        public decimal? Price { get; set; }
         public bool Status{ get; set; }
 
     }
